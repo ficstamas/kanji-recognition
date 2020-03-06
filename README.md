52388d5e8a668945cf911e52853de00bc3c132f3
Tamás Ficsor <ficstamas@gmail.com>
2020-03-06 16:29:58 +0100

    Basic train/test split function, readme update

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 ### Requirements
 - Python 3.7.*
 - Dependencies can be found in the [requirements.txt](requirements.txt)
-- The used dataset can be found om [Kaggle](https://www.kaggle.com/anokas/kuzushiji)
+- PyTorch installation for windows: `pip install torch===1.4.0 torchvision===0.5.0 -f https://download.pytorch.org/whl/torch_stable.html`
+- The used dataset can be found on [Kaggle](https://www.kaggle.com/anokas/kuzushiji)
--- utils/images.py
+++ utils/images.py
@@ -3,6 +3,8 @@ import os
 import numpy as np
 import tqdm
 import logging
+import random
+import math
 
 logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s - %(levelname)s - %(message)s',
@@ -20,16 +22,27 @@ class Kanjis:
     _id = 0
     first_image = True
 
-    def __init__(self):
-        pass
+    def __init__(self, seed=None):
+        random.seed = seed
 
     def add_label(self, label: str):
+        """
+        Add label, it will create conversion dictionary as well
+        :param label:
+        :return:
+        """
         if label not in self.l2i:
             self.l2i[label] = self._id
             self.i2l[self._id] = label
             self._id += 1
 
     def add_image(self, img: np.ndarray, label: str):
+        """
+        Add a single image
+        :param img: [64, 64] image
+        :param label: label
+        :return:
+        """
         if self.first_image:
             self._images[0, :, :] = img
             self._labels[0] = self.l2i[label]
@@ -37,6 +50,30 @@ class Kanjis:
         else:
             self._images = np.concatenate([self._images, [img]], axis=0)
             self._labels = np.concatenate([self._labels, [self.l2i[label]]], axis=0)
+
+    def add_images(self, imgs: np.ndarray, label: str):
+        """
+        Add multiple images with same label
+        :param imgs: [*, 64, 64] images
+        :param label: class label
+        :return:
+        """
+        if self.first_image:
+            self._images = imgs
+            self._labels = np.array([self.l2i[label]]*imgs.shape[0], dtype=np.uint)
+            self.first_image = False
+        else:
+            self._images = np.concatenate([self._images, imgs], axis=0)
+            self._labels = np.concatenate([self._labels, [self.l2i[label]]*imgs.shape[0]], axis=0)
+
+    def train_test_split(self, train_ratio=0.6):
+        mask = np.zeros(self._labels.shape, dtype=np.bool)
+        size = mask.shape[0]
+
+        random_vector = random.sample(range(size), math.ceil(size*train_ratio))
+        mask[random_vector] = 1
+
+        return self._images[mask], self._labels[mask], self._images[~mask], self._labels[~mask]
 
     def __len__(self):
         return self._images.shape[0]
@@ -48,11 +85,11 @@ class Kanjis:
         return self.__str__()
 
 
-def load_images(path="../data/kkanji/kkanji2/", limit=None) -> Kanjis:
+def load_images(path="../data/kkanji/kkanji2/", category_limit=None, minimum_count=5) -> Kanjis:
     """
     Load images and labels into object
     :param path: Path to the folder of the Kanjis
-    :param limit: Maximum number of categories to load
+    :param category_limit: Maximum number of categories to load
     :return:
     """
 
@@ -71,17 +108,28 @@ def load_images(path="../data/kkanji/kkanji2/", limit=None) -> Kanjis:
             continue
         # getting label from folder name
         label = str(root.split("/")[-1])
-        # adding label
-        kanjis.add_label(label)
+
+        images = np.array([np.zeros([64, 64], dtype=np.uint8)], dtype=np.uint8)
         # loading files
         for file in files:
             img = cv.imread(os.path.join(root, file), cv.IMREAD_GRAYSCALE)
-            kanjis.add_image(img, label)
-        # limiting loaded categories
-        if limit is not None and num == limit:
-            break
-        elif limit is not None:
-            num += 1
+            images = np.concatenate([images, [img]], axis=0)
+        # checking if the class has enough samples
+        if images.shape[0] >= minimum_count:
+            # adding label
+            kanjis.add_label(label)
+            kanjis.add_images(images, label)
+            # limiting loaded categories
+            if category_limit is not None and num == category_limit:
+                break
+            elif category_limit is not None:
+                num += 1
 
     logging.info(f"{kanjis.__len__()} kanji under {kanjis.l2i.__len__()} classes is loaded")
     return kanjis
+
+
+if __name__ == '__main__':
+    kanjis = load_images(category_limit=10)
+    x, y, xx, yy = kanjis.train_test_split()
+    print("")
