3244da5f63df87e2abe94d282d877d3b042c4ce3
Takács Erik <erik29800@gmail.com>
2020-05-01 16:42:50 +0200

    hog and accuracy

--- models/Naive_Bayes.py
+++ models/Naive_Bayes.py
@@ -1,24 +1,24 @@
-from sklearn.svm import LinearSVC
-from sklearn.metrics import classification_report
+from sklearn.naive_bayes import GaussianNB
 from utils.images import load_images
 from sklearn import metrics
 import logging
 from preprocessors.baseline import ravel_data
 
+
+
 kanjis = load_images('C:\\Users\\HealthTeam\\Desktop\\kanji\\kkanji\\kkanji2',
                      minimum_count=5, random_seed=0, category_limit=None)
 x_train, y_train, x_test, y_test = kanjis.train_test_split(0.6)
 
-model = LinearSVC()
-model.fit(ravel_data(x_train), y_train)
+gnb = GaussianNB()
+gnb.fit(ravel_data(x_train), y_train)
 
-predictions = model.predict(ravel_data(x_test))
-test_acc = metrics.cohen_kappa_score(y_test, predictions)
+pred = gnb.predict(ravel_data(x_test))
 
-print('Accuracy Score:', metrics.accuracy_score(y_test,predictions))
+test_acc = metrics.cohen_kappa_score(y_test, pred)
+
+print('Accuracy Score:', metrics.accuracy_score(y_test,pred))
 logging.info(f"Test accuracy: {test_acc}")
 
-file = open('../result/svm/acc.txt','w')
+file = open('../results/Naive_Bayes/acc.txt','w')
 file.write(str(test_acc))
-
-#print(classification_report(y_test, predictions))
